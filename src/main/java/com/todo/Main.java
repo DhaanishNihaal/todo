7f54d5733304cf58b83c14b872528477812832da
DhaanishNihaal <kit27.am13@gmail.com>
2025-09-16 14:04:39 +0530

    DB connection

--- src/main/java/com/todo/Main.java
+++ src/main/java/com/todo/Main.java
@@ -1,7 +1,18 @@
 package com.todo;
+import com.todo.util.DatabaseConnection;
+import java.sql.SQLException;
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
-        
+        DatabaseConnection db_Connection=new DatabaseConnection();
+    try{
+        Connection cn=db_Connection.getDBConnection();
+        System.out.println("Connection Successful");
+    }
+    catch(SQLException e){
+        System.out.println("Connection Failed");
+    }
     }
     
 }
